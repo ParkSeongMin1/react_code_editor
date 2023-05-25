c7c4303040222d4900bc82cdfbcb33152f7cdcf9
SeongMin <smite010@g.skku.edu>
2023-05-26 01:22:16 +0900

    create project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Landing from "./components/Landing";
 
 function App() {
-  return <Landing />;
+  return (
+  <div>
+    <Landing />
+  </div>
+  );
 }
 
 export default App;
